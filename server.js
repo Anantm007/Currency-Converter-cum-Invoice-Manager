e566198feb3f732a1e5af1bd24c6e511f1bbeab7
Sakshi2k <sakshi2k@yahoo.co.in>
2020-04-27 13:07:07 +0530

    styling done

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // Setting express engine
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static("views"));
 
 
 
