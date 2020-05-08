ebb36f2d66ef109c6505a09c573090695d4ffd2e
Anant Mathur <anant.mathur007@gmail.com>
2020-05-08 23:37:47 +0530

    models finalised

--- models/Client.js
+++ models/Client.js
@@ -1,23 +1,30 @@
 const mongoose = require("mongoose");
 
-const clientSchema = mongoose.Schema({
+const clientSchema = new mongoose.Schema({
     name :{
-        type : String
+        type : String,
+        required: true,
+        trim: true
+    },
+
+    companyName: {
+        type : String,
+        trim: true,
+        default: 'N/A'
     },
 
     clientId : {
-        type : String
+        type : String,
+        required: true
     },
 
-    invoices : {
-        type : [String]
-    },
-},
-    
-    { timestamps: true }
-)
+    invoices : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Invoice'
+    }],
+
+},  { timestamps: true }
+
+);
 
 module.exports = Client = mongoose.model("Client", clientSchema);
-
-
-// module.export = clientSchema
