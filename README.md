6b10d83175e44690929934d7f21061b81ae8e8d1
Anant Mathur <anant.mathur007@gmail.com>
2020-07-14 15:24:27 +0530

    remove freeCurrencyConverter API and add foreign exchange api by central european bank

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Currency-Converter
-This project consists of 2 parts: Currency converter for EUR, USD and INR (built using free currencyConverterAPI) and Invoice Manager (where we can perform CRUD operations for clients and invoices). This project has a sumarry section for the invoices as well. It can be used by freelancers to keep a record of their clients and invoices in a fool proof manner. This web application is built using Node.js and EJS.
+# Currency Converter cum Invoice Manager
+This project consists of 2 parts: 
+* Currency converter for EUR, USD and INR (built using Foreign exchange rates API, published by the European Central Bank)
+* Invoice Manager (where we can perform CRUD operations for clients and invoices). This project has a sumarry section for the invoices as well. It can be used by freelancers to keep a record of their clients and invoices in a fool proof manner. 
+###### This web application is built using Node.js and EJS.
--- routes/converter.js
+++ routes/converter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express();
 
 // Utility packages
-const https = require('https');
-const moment = require('moment-timezone')
+const axios = require('axios');
+const moment = require('moment-timezone');
 require('dotenv').config();
 
 
@@ -35,130 +35,59 @@ router.get('/', async(req, res) => {
 // @desc    Call convert money function and return the converted amounts
 // @access  Public
 router.post('/convert', async(req, res) => {
+    const { amount, fromCurrency } = req.body;
+    const url = `https://api.exchangeratesapi.io/latest?base=${fromCurrency}`;
+    var inr, usd, eur;
     
-    var fromCurrency = req.body.fromCurrency;
-    var amount = req.body.amount;
-    var apiKey = process.env.apiKey;
-    var toCurrency;
-
-    let eur, inr, usd;
-    
-    var query;
-
-    if(fromCurrency === 'USD')  // If currency is USD
-    {
-        toCurrency = 'EUR';
-        query = fromCurrency + '_' + toCurrency;
-
-        await convertCurrency(apiKey, amount, query, function(err, total) {   
-            eur = total;
-    
-            toCurrency = 'INR';
-            query = fromCurrency + '_' + toCurrency;
-
-            convertCurrency(apiKey, amount, query, function(err, total) {
-                inr = total;        
-                return res.render("../views/response",{
-                    usd: Number(amount).toFixed(2),
-                    inr: inr.toFixed(2),
-                    eur: eur.toFixed(2)
-                })
-        
-            });
-
-        });
+    if(fromCurrency === "USD") {
+        axios.get(url)
+        .then(data => {
+            inr = data.data.rates.INR * Number(amount);
+            eur = data.data.rates.EUR * Number(amount);
+            return res.render("../views/response",{
+                usd: Number(amount).toFixed(2),
+                inr: inr.toFixed(2),
+                eur: eur.toFixed(2)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
-    else if(fromCurrency === 'EUR') // If currency is EUR
-    {
-
-        toCurrency = 'USD';
-        query = fromCurrency + '_' + toCurrency;
-
-        await convertCurrency(apiKey, amount, query, function(err, total) {   
-            usd = total;
-    
-            toCurrency = 'INR';
-            query = fromCurrency + '_' + toCurrency;
-
-            convertCurrency(apiKey, amount, query, function(err, total) {
-                inr = total;        
-                return res.render("../views/response",{
-                    usd: usd.toFixed(2),
-                    inr: inr.toFixed(2),
-                    eur: Number(amount).toFixed(2)
-                })
-        
-            });
-
-        });
-    }
-    
-    
-    else if(fromCurrency === 'INR') // If currency is INR
-    {
-        toCurrency = 'USD';
-        query = fromCurrency + '_' + toCurrency;
-
-        await convertCurrency(apiKey, amount, query, function(err, total) {   
-            usd = total;
-    
-            toCurrency = 'EUR';
-            query = fromCurrency + '_' + toCurrency;
-
-            convertCurrency(apiKey, amount, query, function(err, total) {
-                eur = total;        
-                return res.render("../views/response",{
-                    usd: usd.toFixed(2),
-                    eur: eur.toFixed(2),
-                    inr: Number(amount).toFixed(2)
-                })
-        
-            });
-
-        });
+    if(fromCurrency === "EUR") {
+        axios.get(url)
+        .then(data => {
+            inr = data.data.rates.INR * Number(amount);
+            usd = data.data.rates.USD * Number(amount);
+            return res.render("../views/response",{
+                eur: Number(amount).toFixed(2),
+                inr: inr.toFixed(2),
+                usd: usd.toFixed(2)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
+    if(fromCurrency === "INR") {
+        axios.get(url)
+        .then(data => {
+            usd = data.data.rates.USD * Number(amount);
+            eur = data.data.rates.EUR * Number(amount);
+            return res.render("../views/response",{
+                inr: Number(amount).toFixed(2),
+                usd: usd.toFixed(2),
+                eur: eur.toFixed(2)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    
 })
 
-
-// Function that uses free currency converter API to convert money amount
-convertCurrency = (apiKey, amount, query, cb) => {
-    
-    var url = 'https://free.currconv.com/api/v7/convert?q=' + query + '&compact=ultra&apiKey=' + apiKey;
-    
-    https.get(url, function(res){
-    var body = '';
-
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-
-    res.on('end', function(){
-        try {
-          var jsonObj = JSON.parse(body);
-
-          var val = jsonObj[query];
-          if (val) {
-            total = val * amount;
-            console.log(total);
-            cb(null,total);
-          } else {
-            var err = new Error("Value not found for " + query);
-            console.log(err);
-            cb(err, 0)
-          }
-        } catch(e) {
-          console.log("Parse error: ", e);
-          cb(e, 0);
-        }
-    });
-
-
-    }).on('error', function(e){
-        console.log("Got an error: ", e);
-        cb(e);
-    });
-}
 
 module.exports = router;
